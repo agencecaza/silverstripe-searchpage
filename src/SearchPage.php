4637dafda86a3989782e95f2198ecd6db1299a70
Stéphane Guevremont <stephane.guevremont@gmail.com>
2019-09-29 22:42:28 -0400

    Another enhancement
    Remove words containing 2 or less caracters from the results.

--- src/SearchPage.php
+++ src/SearchPage.php
@@ -10,148 +10,148 @@ use SilverStripe\ORM\PaginatedList;
 use SilverStripe\Versioned\Versioned;
 use SilverStripe\Security\Member;
 
-class SearchPage extends Page {
-
-	private static $description = 'Search page';
-
+class SearchPage extends Page
+{
+    private static $description = 'Search page';
 }
 
-class SearchPageController extends PageController {
+class SearchPageController extends PageController
+{
+    private static $allowed_actions = array(
+        'SearchForm'
+    );
 
-	private static $allowed_actions = array (
-		'SearchForm'
-	);
+    public function SearchForm()
+    {
+        $form = Form::create(
+            $this,
+            'SearchForm',
+            FieldList::create(
+                TextField::create('Keywords', '')
+                        ->setAttribute('placeholder', _t('SearchPage.KEYWORDS', 'Keywords...'))
+            ),
+            FieldList::create(
+                FormAction::create('SearchFormSubmit', _t('SearchPage.SEARCH', 'Search'))
+            )
+        );
+        $form->setFormMethod('GET')
+                 ->disableSecurityToken();
 
-	public function SearchForm() {
-
-		$form = Form::create(
-			$this,
-			'SearchForm',
-			FieldList::create(
-					TextField::create('Keywords','')
-						->setAttribute('placeholder', _t('SearchPage.KEYWORDS','Keywords...'))
-			),
-			FieldList::create(
-					FormAction::create('SearchFormSubmit', _t('SearchPage.SEARCH','Search'))
-			)
-		);
-		$form->setFormMethod('GET')
-				 ->disableSecurityToken();
-
-		return $form;
-
-	}
-
-
-public function SearchFormSubmit($data, $form) {
+        return $form;
+    }
 
 
-		$rank[0] = 0;
+    public function SearchFormSubmit($data, $form)
+    {
+        $rank[0] = 0;
 
-		$strings = array(
-		);
+        $strings = array(
+        );
 
-		/*
-		*	Check if exist a perfect match and add a first rank
-		*
-		*/
-		$pages = Versioned::get_by_stage('Page','Live')->filterAny(
-			array(
-				'Title:PartialMatch:nocase' => $data['Keywords'],
-				'Content:PartialMatch:nocase' => $data['Keywords'],
-				'ContentSearch:PartialMatch:nocase' => $data['Keywords'],
-			)
-		);
-		if ($pages) {
-			foreach ($pages as $page) {
-				if ($page) {
-					if (!isset($rank[$page->ID])) { $rank[$page->ID]=0; }
-					$rank[$page->ID] ++;
-				}
-			}
-		}
+        /*
+        *	Check if exist a perfect match and add a first rank
+        *
+        */
+        $pages = Versioned::get_by_stage('Page', 'Live')->filterAny(
+            array(
+                'Title:PartialMatch:nocase' => $data['Keywords'],
+                'Content:PartialMatch:nocase' => $data['Keywords'],
+                'ContentSearch:PartialMatch:nocase' => $data['Keywords'],
+            )
+        );
+        if ($pages) {
+            foreach ($pages as $page) {
+                if ($page) {
+                    if (!isset($rank[$page->ID])) {
+                        $rank[$page->ID]=0;
+                    }
+                    $rank[$page->ID] ++;
+                }
+            }
+        }
 
-		/*
-		*	Check every words submitted
-		*
-		*/
+        /*
+        *	Check every words submitted and remove words containing 2 caracters or less. Return less results.
+        *
+        */
+        $keywords = explode(" ", str_replace($strings, "", $data['Keywords']));
 
-		$keywords = explode(" ", str_replace($strings, "", $data['Keywords']));
+        foreach ($keywords as $value => $val) {
+            if (strlen($val) > 2) {
 
-		foreach ($keywords as $value => $val) {
+                $pages = Versioned::get_by_stage('Page', 'Live')->filterAny(
+                    array(
+                        'Title:PartialMatch:nocase' => $val,
+                        'Content:PartialMatch:nocase' => $val,
+                        'ContentSearch:PartialMatch:nocase' => $val,
+                    )
+                );
 
-			$pages = Versioned::get_by_stage('Page','Live')->filterAny(
-				array(
-					'Title:PartialMatch:nocase' => $val,
-					'Content:PartialMatch:nocase' => $val,
-					'ContentSearch:PartialMatch:nocase' => $val,
-				)
-			);
+                if ($pages) {
+                    foreach ($pages as $page) {
+                        if ($page) {
+                            if (!isset($rank[$page->ID])) {
+                                $rank[$page->ID]=0;
+                            }
+                            $rank[$page->ID] ++;
+                        }
+                    }
+                }
+            }
+        }
 
-			if ($pages) {
-				foreach ($pages as $page) {
+        /*
+        * Check pages add per rank.
+        *
+        */
+        $results = new ArrayList();
 
-					if ($page) {
-						if (!isset($rank[$page->ID])) { $rank[$page->ID]=0; }
-						$rank[$page->ID] ++;
-					}
-				}
-			}
-		}
+        foreach ($rank as $value => $key) {
+            $page = Versioned::get_by_stage('Page', 'Live')->exclude('ClassName', 'SilverStripe\ErrorPage\ErrorPage')->filter('ID', $value)->first();
 
-		/*
-		* Check pages add per rank.
-		*
-		*/
-		$results = new ArrayList();
+            if ($page) {
+                $results->push(
+                    ArrayData::create(
+                        array(
+                            'Title' => $page->Title,
+                            'Link' => $page->Link(),
+                            'Rank' => $rank[$value],
+                        )
+                    )
+                );
+            }
+        }
 
-		foreach ($rank as $value => $key) {
-
-			$page = Versioned::get_by_stage('Page','Live')->exclude('ClassName','SilverStripe\ErrorPage\ErrorPage')->filter('ID', $value)->first();
-
-			if ($page) {
-				$results->push(
-					ArrayData::create(
-						array(
-							'Title' => $page->Title,
-							'Link' => $page->Link(),
-							'Rank' => $rank[$value],
-						)
-					)
-				);
-			}
-		}
-
-		/*
-		* Sort higher is the score on top.
-		*
-		*/
-		$resultslist = $results->sort('Rank')->reverse();
+        /*
+        * Sort higher is the score on top.
+        *
+        */
+        $resultslist = $results->sort('Rank')->reverse();
 
 
-		/*
-		* Create a paginated pages results
-		*
-		*/
-		$paginatedProperties = PaginatedList::create(
-   		$resultslist,
-   		$this->getRequest()
-		)->setPageLength(25);
+        /*
+        * Create a paginated pages results
+        *
+        */
+        $paginatedProperties = PaginatedList::create(
+            $resultslist,
+            $this->getRequest()
+        )->setPageLength(25);
 
-		return array (
-			'Results' => $paginatedProperties
-		);
+        return array(
+            'Results' => $paginatedProperties
+        );
+    }
 
-	}
+    public function Keywords()
+    {
+        if (empty($_GET['Keywords'])) {
+            return true;
+        }
+    }
 
-	public function Keywords() {
-		if (empty($_GET['Keywords'])) {
-			return true;
-		}
-	}
-
-	public function KeywordsGet() {
-		return $_GET['Keywords'];
-	}
-
+    public function KeywordsGet()
+    {
+        return $_GET['Keywords'];
+    }
 }
